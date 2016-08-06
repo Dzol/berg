eafd9ed13f5a45362918467f94e1a2d6086f53f5
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2016-08-06 12:08:55 +0200

    Initial commit with test suite

---
+++ README.md
@@ -0,0 +1,8 @@
+# Heap
+
+A Minimum Heap for Integers in Erlang.
+Google or find _Pairing Heap_ in Wikipedia.
+
+## Interesting Make Targets
+
+Test with `make test` and build the documentaion with `make doc`.
---
+++ src/heap.erl
@@ -0,0 +1,72 @@
+%% ===================================================================
+%% @copyright 2016 Joseph Yiasemides
+%% @author <joseph.yiasemides@erlang-solutions.com>
+%% @doc A <b>Minimum</b> Heap for <b>Integers</b> in Erlang.
+%% @reference Google "Pairing Heap" or find it in Wikipedia.
+%% @end
+%% ===================================================================
+
+-module(heap).
+-export([zero/0]).
+-export([zero/1]).
+-export([peak/1]).
+-export([insert/2]).
+-export([extract/1]).
+
+%% Our heap is a structure with an integer value and a list of
+%% children (heaps). Invarient: the integer value of any heap is
+%% smaller than those of its children. We really want something like
+%% `heap of integer :: {value of integer, [] | [heap of integer]}',
+%% but since Erlang's Dialyzer doesn't permit inductive structures,
+%% let's take the symbol `infinity' to be a value outside of the
+%% permitable integer values.
+-opaque heap() :: {infinity | integer(), list()}.
+
+
+%% -------------------------------------------------------------------
+%% Interface
+%% -------------------------------------------------------------------
+
+%% @doc The zero value of a heap.
+-spec zero() -> heap().
+zero()       -> {infinity, []}.
+
+%% @doc Predicate: is this the zero value of a heap? It'll fail for
+%% something that does not have the structure of a heap. True if it is
+%% a zero heap. False if the heap has cardinality greater than or
+%% equal to one.
+-spec zero(heap())                          -> boolean().
+zero({infinity, []})                        -> true;
+zero({V, C}) when is_integer(V), is_list(C) -> false.
+
+%% @doc The heap with the given integer value inserted onto the given
+%% heap.
+-spec insert(heap(), integer()) -> heap().
+insert(H, Y) when is_integer(Y) -> merge(H, make(Y)).
+
+%% @doc The leading value kept on this heap. Named `peak' in the sense
+%% that it is the value at the <i>peak</i> of the given heap.
+-spec peak(heap())                                  -> integer().
+peak({Value, C}) when is_integer(Value), is_list(C) -> Value.
+
+%% @doc Extract the smallest value off of the given heap.
+-spec extract(heap())                       -> {heap(), integer()}.
+extract({Value, []}) when is_integer(Value) -> {zero(), Value};
+extract({Value, Children})
+  when is_integer(Value), is_list(Children) -> {pair(Children), Value}.
+
+
+%% -------------------------------------------------------------------
+%% Ancillary
+%% -------------------------------------------------------------------
+
+pair([])         -> zero();
+pair([H])        -> H;
+pair([I,J|Rest]) -> merge(merge(I, J), pair(Rest)).
+
+merge({infinity, []}, H)              -> H;
+merge(H, {infinity, []})              -> H;
+merge({X, U}, {Y, _} = H) when X < Y  -> {X, [H|U]};
+merge({X, _} = H, {Y, V}) when Y =< X -> {Y, [H|V]}.
+
+make(Value) -> {Value, []}.
