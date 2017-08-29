ca3e1feecdbef0da644e9533685aab1793c1abfd
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2017-08-29 23:04:09 +0200

    Name I/O variables more explicitily

--- property/berg_property.exs
+++ property/berg_property.exs
@@ -4,42 +4,42 @@ defmodule BergProperty do
   import Berg, only: [heapify: 1, listify: 1]
 
   property "first element out is the smallest in" do
-    check all x <- list_of(integer()), x != [] do
-      y = x |> heapify() |> listify()
+    check all input <- list_of(integer()), input != [] do
+      out = input |> heapify() |> listify()
 
-      assert List.first(y) === Enum.min(x)
+      assert List.first(out) === Enum.min(input)
     end
   end
 
   property "last element out is the biggest in" do
-    check all x <- list_of(integer()), x != [] do
-      y = x |> heapify() |> listify()
+    check all input <- list_of(integer()), input != [] do
+      out = input |> heapify() |> listify()
 
-      assert List.last(y) == Enum.max(x)
+      assert List.last(out) == Enum.max(input)
     end
   end
 
   property "as many elements out as put in" do
-    check all x <- list_of(integer()), x != [] do
-      y = x |> heapify() |> listify()
+    check all input <- list_of(integer()), input != [] do
+      out = input |> heapify() |> listify()
 
-      assert length(y) == length(x)
+      assert length(out) == length(input)
     end
   end
 
   property "output values ⊆ input values" do
-    check all x <- list_of(integer()), x != [] do
-      y = x |> heapify() |> listify()
+    check all input <- list_of(integer()), input != [] do
+      out = input |> heapify() |> listify()
 
-      assert Enum.all?(y, &Enum.member?(x, &1))
+      assert Enum.all?(out, &Enum.member?(input, &1))
     end
   end
 
   property "input values ⊆ output values" do
-    check all x <- list_of(integer()), x != [] do
-      y = x |> heapify() |> listify()
+    check all input <- list_of(integer()), input != [] do
+      out = input |> heapify() |> listify()
 
-      assert Enum.all?(x, &Enum.member?(y, &1))
+      assert Enum.all?(input, &Enum.member?(out, &1))
     end
   end
 
